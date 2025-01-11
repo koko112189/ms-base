8cb30ecaff01824dfa70d071e99d4058cb832404
John <John@DESKTOP-OPFHU6D>
2025-01-10 22:13:51 -0500

    feat: added folder structure
    added folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { ControllerModule } from './controller/controller.module';
+import { CoreModule } from './core/core.module';
+import { DataProviderModule } from './data-provider/data-provider.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommonModule, ControllerModule, CoreModule, DataProviderModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
